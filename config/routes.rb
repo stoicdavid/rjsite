f7e228992b56fd1c303877a1c9e3a133e0540052
David Rodriguez <stoicdavid@gmail.com>
2016-08-15 03:06:09 -0500

    Aviso de privacidad

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :quotes
   get 'landing/index'
 
+  get 'aviso/index'
+  get 'aviso' => 'aviso#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
